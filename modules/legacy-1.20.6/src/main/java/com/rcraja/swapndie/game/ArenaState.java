7623244cb4d614e86124d57eef4225cc61206a73
imrcraja <indoadvance3@gmail.com>
2026-09-18 00:28:13 +0530

    Update SwapNDie project

--- modules/legacy-1.20.6/src/main/java/com/rcraja/swapndie/game/ArenaState.java
+++
@@ -1,8 +0,0 @@
-package com.rcraja.swapndie.game;
-
-public enum ArenaState {
-    BUILDING,   // creator is placing the trap, spawn/win points not both set yet
-    ARMED,      // both points set, countdown timer running
-    ACTIVE,     // opponent has been swapped in, round is live
-    FINISHED    // round ended (win or loss)
-}
